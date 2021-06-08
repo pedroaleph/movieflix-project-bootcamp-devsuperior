72691bd5f40f4006417fff379cb687dc025d2714
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-06-08 12:55:18 -0400

    pasting services exceptions, user service and auth service from dslearn

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.movieflix.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	public EntityNotFoundException (String msg) {
+		super(msg);
+	}
+}
