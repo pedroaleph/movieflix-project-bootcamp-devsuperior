f6337b0cc32a675328801afc8cb3a7b18cfe8673
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-06-08 20:27:14 -0400

    movie resource find all paged

--- backend/src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
+++ backend/src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -1,10 +1,13 @@
 package com.devsuperior.movieflix.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
@@ -16,6 +19,19 @@ public class MovieResource {
 	
 	@Autowired
 	private MovieService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<MovieDTO>> findAllPaged(
+			@RequestParam(value = "genreId", defaultValue = "0") Long genreId,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "12") Integer size
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page, size);
+		Page<MovieDTO> list = service.findAllPaged(genreId, pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<MovieDTO> findById(@PathVariable Long id){
