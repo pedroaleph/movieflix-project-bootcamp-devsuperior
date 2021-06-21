9a59f0b04c5703f37e05921d03678485b840d70a
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-06-21 19:54:54 -0400

    add navbar

--- front-web/src/core/components/NavBar/index.tsx
+++ front-web/src/core/components/NavBar/index.tsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const NavBar = () => {
   return (
-    <div></div>
+    <div className="bg-primary nav-main">
+      <Link to="/" className="nav-logo-text text-dark">
+        <h4>MovieFlix</h4>
+      </Link>
+      <button type="button" className="btn btn-outline-dark">
+        <h5>SAIR</h5>
+      </button>
+    </div>
   )
 }
 
