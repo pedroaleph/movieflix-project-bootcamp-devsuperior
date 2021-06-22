f253f8215f7bd99d397117e30a7dc0394c0ef4d6
Pedro Aleph <pedro.aleph.allen@gmail.com>
2021-06-21 20:16:07 -0400

    add navbar media 576px

--- front-web/src/core/components/NavBar/index.tsx
+++ front-web/src/core/components/NavBar/index.tsx
@@ -1,13 +1,24 @@
+import { logout } from 'core/utils/auth';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const NavBar = () => {
+
+  const handleLogout = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    logout();
+  }
+
   return (
     <div className="bg-primary nav-main">
       <Link to="/" className="nav-logo-text text-dark">
         <h4>MovieFlix</h4>
       </Link>
-      <button type="button" className="btn btn-outline-dark">
+      <button
+        type="button"
+        className="btn btn-outline-dark"
+        onClick={event => handleLogout(event)}
+      >
         <h5>SAIR</h5>
       </button>
     </div>
